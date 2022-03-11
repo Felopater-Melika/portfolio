6758bddbdc20af3a8d110570730a80c4a1e33773
Felopater <akira.archangel2099@gmail.com>
2022-03-11 09:15:16 -0600

    initial commit

---
+++ components/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <h1>I am Felopater,</h1>
+      <h2>Self-taught front-end web developer</h2>
+      <h4>
+        I build websites that focus on great user experience and interface that
+        attracts customers and grows your business.
+      </h4>
+    </main>
+  )
+}
+
+export default Home
